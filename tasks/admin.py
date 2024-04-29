ece829a8c1c886b70b0b3c6daa1dbb3c0b680c0b
Mr Bugy <aleksey.serzhuk@mail.ru>
2024-04-29 21:51:17 +0300

    Lesson 5

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,43 @@
-from django.contrib import admin
+"""Module docstring"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Project, Task
+
+
+class TaskInline(admin.TabularInline):
+    """
+    Inline класс для модели Task
+    """
+    model = Task
+    extra = 0
+    fields = ('name', 'description', 'assignee', 'status', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    can_delete = True
+    show_change_link = True
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """
+    Класс администратора для модели Project
+    """
+
+    list_display = ('name', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+
+    inlines = [TaskInline]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """
+    Класс администратора для модели Project
+    """
+
+    list_display = ('name', 'project', 'assignee', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'assignee', 'project')
+    search_fields = ('name', 'description')
+    list_editable = ('status', 'assignee')
+    readonly_fields = ('created_at', 'updated_at')
