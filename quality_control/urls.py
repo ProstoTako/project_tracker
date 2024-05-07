fb22884d6711ad8ff25068041edb3c947260243e
Mr Bugy <aleksey.serzhuk@mail.ru>
2024-05-07 18:24:39 +0300

    HW 10

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -10,8 +10,12 @@ urlpatterns = [
     path('bugs/', views.bug_list, name='bug_list'),
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
     path('bugs/new/', views.create_bug_report, name='create_bug_report'),
+    path('bugs/<int:bug_id>/update/', views.update_bug_report, name='update_bug_report'),
+    path('bugs/<int:bug_id>/delete/', views.delete_bug_report, name='delete_bug_report'),
 
     path('features/', views.feature_list, name='feature_list'),
     path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
     path('features/new/', views.create_feature_request, name='create_feature_request'),
+    path('features/<int:feature_id>/update/', views.update_feature_request, name='update_feature_request'),
+    path('features/<int:feature_id>/delete/', views.delete_feature_request, name='delete_feature_request'),
 ]
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.views.generic import DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import DetailView, CreateView
 from .models import BugReport, FeatureRequest
 from .forms import BugReportForm, FeatureRequestForm
 
@@ -52,3 +52,41 @@ def create_feature_request(request):
         form = FeatureRequestForm()
 
     return render(request, 'quality_control/feature_request_form.html', {'form': form})
+
+
+def update_bug_report(request, bug_id):
+    bug = get_object_or_404(BugReport, pk=bug_id)
+    if request.method == 'POST':
+        form = BugReportForm(request.POST, instance=bug)
+        if form.is_valid():
+            form.save()
+            return redirect('q_control:bug_detail', bug_id=bug.id)
+    else:
+        form = BugReportForm(instance=bug)
+    return render(request, 'quality_control/bug_report_update.html', {'form': form, 'bug': bug})
+
+
+def update_feature_request(request, feature_id):
+    feature = get_object_or_404(FeatureRequest, pk=feature_id)
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST, instance=feature)
+        if form.is_valid():
+            form.save()
+            return redirect('q_control:feature_detail', feature_id=feature.id)
+    else:
+        form = FeatureRequestForm(instance=feature)
+    return render(request, 'quality_control/feature_request_update.html', {'form': form, 'feature': feature})
+
+
+def delete_bug_report(request, bug_id):
+    bug = get_object_or_404(BugReport, pk=bug_id)
+    bug.delete()
+
+    return redirect('q_control:bug_list')
+
+
+def delete_feature_request(request, feature_id):
+    feature = get_object_or_404(FeatureRequest, pk=feature_id)
+    feature.delete()
+
+    return redirect('q_control:feature_list')
