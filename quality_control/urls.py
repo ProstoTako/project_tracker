38652a7811f23b02288d8ecf4e5263a404818aa6
Mr Bugy <aleksey.serzhuk@mail.ru>
2024-05-07 14:21:40 +0300

    HW 9

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('bugs/', views.bug_list, name='bug_list'),
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
+    path('bugs/new/', views.create_bug_report, name='create_bug_report'),
+
     path('features/', views.feature_list, name='feature_list'),
     path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
+    path('features/new/', views.create_feature_request, name='create_feature_request'),
 ]
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import BugReport, FeatureRequest
+from .forms import BugReportForm, FeatureRequestForm
 
 
 def index(request):
@@ -27,3 +28,27 @@ class FeatureDetailView(DetailView):
     model = FeatureRequest
     pk_url_kwarg = 'feature_id'
     template_name = 'quality_control/feature_detail.html'
+
+
+def create_bug_report(request):
+    if request.method == 'POST':
+        form = BugReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('q_control:bug_list')
+    else:
+        form = BugReportForm()
+        
+    return render(request, 'quality_control/bug_report_form.html', {'form': form})
+
+
+def create_feature_request(request):
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('q_control:feature_list')
+    else:
+        form = FeatureRequestForm()
+
+    return render(request, 'quality_control/feature_request_form.html', {'form': form})
