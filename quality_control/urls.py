fd8a1f5b551b0df860e98ae735e9b146ce8e9fea
Mr Bugy <aleksey.serzhuk@mail.ru>
2024-05-07 23:02:02 +0300

    HW 10: CBV

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -7,15 +7,15 @@ app_name = 'q_control'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('bugs/', views.bug_list, name='bug_list'),
+    path('bugs/', views.BugListView.as_view(), name='bug_list'),
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
-    path('bugs/new/', views.create_bug_report, name='create_bug_report'),
-    path('bugs/<int:bug_id>/update/', views.update_bug_report, name='update_bug_report'),
-    path('bugs/<int:bug_id>/delete/', views.delete_bug_report, name='delete_bug_report'),
+    path('bugs/new/', views.BugCreateView.as_view(), name='create_bug_report'),
+    path('bugs/<int:bug_id>/update/', views.BugUpdateView.as_view(), name='update_bug_report'),
+    path('bugs/<int:bug_id>/delete/', views.BugDeleteView.as_view(), name='delete_bug_report'),
 
-    path('features/', views.feature_list, name='feature_list'),
+    path('features/', views.FeatureListView.as_view(), name='feature_list'),
     path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
-    path('features/new/', views.create_feature_request, name='create_feature_request'),
-    path('features/<int:feature_id>/update/', views.update_feature_request, name='update_feature_request'),
-    path('features/<int:feature_id>/delete/', views.delete_feature_request, name='delete_feature_request'),
+    path('features/new/', views.FeatureCreateView.as_view(), name='create_feature_request'),
+    path('features/<int:feature_id>/update/', views.FeatureUpdateView.as_view(), name='update_feature_request'),
+    path('features/<int:feature_id>/delete/', views.FeatureDeleteView.as_view(), name='delete_feature_request'),
 ]
